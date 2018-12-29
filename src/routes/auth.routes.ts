21e1e17265830a8a0eec6a165e9ae01eb205791f
Aravind Raj <aravinddevaraj777@gmail.com>
2018-12-29 10:38:18 +0530

    verify

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -3,9 +3,6 @@ import { LoginComponent } from 'src/app/component/auth/login/login.component';
 import { RegisterComponent } from 'src/app/component/auth/register/register.component';
 import { RegisterVerifyComponent } from 'src/app/component/auth/register/register_verify/register_verify.component';
 
-
-
-
 export const AUTH_ROUTE: Routes = [
      { path: 'login', component: LoginComponent },
      { path: 'register', component: RegisterComponent },
