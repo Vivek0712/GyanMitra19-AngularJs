c4067de22c718dba819a79c4641b626acb6d4866
Aravind S <saravind23399@gmail.com>
2019-01-03 00:35:57 +0530

    Finshed Workshop Cart
    Aravind S

--- src/app/component/user/cart/cart.component.ts
+++ src/app/component/user/cart/cart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { EventRegistrationService } from 'src/app/services/eventRegistration/event-registration.service';
+
+declare var M: any;
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-
-  constructor() { }
+  workshops : any;
+  user_id : string;
+  constructor(private eventRegistrationService: EventRegistrationService) { 
+    this.workshops = []
+    this.user_id = JSON.parse(localStorage.getItem('user')).id;
+  }
 
   ngOnInit() {
+    this.getWorkshops();
+  }
+
+  cancelWorkshopRegistration(_id: string){
+    this.eventRegistrationService.cancelWorkshopRegistration(this.user_id).subscribe((response: any)=>{
+      if(response.error == true){
+        M.toast({ html: response.msg, classes: 'roundeds danger' });
+      }
+      else{
+        this.workshops = response;
+      }
+    })
+  }
+
+  getWorkshops(){
+    this.eventRegistrationService.getWorkshops(this.user_id).subscribe((response: any)=>{
+      if(response.error == true){
+        M.toast({ html: response.msg, classes: 'roundeds danger' });
+      }
+      else{
+        this.workshops = response;
+      }
+    })
   }
 
 }
--- src/app/services/eventRegistration/event-registration.service.ts
+++ src/app/services/eventRegistration/event-registration.service.ts
@@ -49,20 +49,15 @@ export class EventRegistrationService {
     return this.http.get(this.app.getUrl(this.baseUrl) + 'checkWorkshopRegistration/' + user_id + "/" + event_id);
   }
 
-  // readParticipationStatus(page: any) {
-  //   const headers = new Headers();
-  //   headers.append('Content-Type', 'application/json');
-  //   return this.http.get(this.app.getUrl(this.baseUrl)+"?page="+page);
-  // }
-  // updateParticipationStatus(id: String, name: String,score: String) {
-  //   const body = { _id: id , name: name,score: score };
-  //   const headers = new Headers();
-  //   headers.append('Content-Type', 'application/json');
-  //   return this.http.put(this.app.getUrl(this.baseUrl + `${id}`), body).pipe(map(res => res, {'headers': headers}));
-  // }
-  // deleteParticipationStatus(id: String) {
-  //   const headers = new Headers();
-  //   headers.append('Content-Type', 'application/json');
-  //   return this.http.delete(this.app.getUrl(this.baseUrl + `${id}` ) ).pipe(map(res => res, {'headers': headers}));
-  // }
+  getWorkshops(user_id: string){
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.get(this.app.getUrl(this.baseUrl) + user_id + "/Workshop");
+  }
+
+  cancelWorkshopRegistration(_id: string){
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.delete(this.app.getUrl(this.baseUrl) + _id);
+  }
 }
