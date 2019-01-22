38644de1d5d5b41490a2cc0c44b5445972c89380
Aravind Raj <aravinddevaraj777@gmail.com>
2019-01-23 01:24:20 +0530

    commit
    AravindRaj

--- src/app/pipes/searchfilter.pipe.ts
+++ src/app/pipes/searchfilter.pipe.ts
@@ -7,16 +7,23 @@ export class SearchfilterPipe implements PipeTransform {
   transform(items: any[], searchText: string): any[] {
     if(!items) return [];
     if(!searchText) return items;
-
+    let values:Boolean;
     searchText = searchText.toLowerCase();
-
     return items.filter( it => {
       if(it.name != undefined){
-        return it.name.toLowerCase().includes(searchText);
+        values = it.name.toLowerCase().includes(searchText);
       }
-      else if (it.title != undefined){
-        return it.title.toLowerCase().includes(searchText);
+      if (it.title != undefined && !values){
+        values = it.title.toLowerCase().includes(searchText);
       }
+      if (it.email_id != undefined && !values) {
+        values = it.email_id.toLowerCase().includes(searchText);
+      }
+      if (it.mobile_number != undefined && !values) {
+        values =  it.mobile_number.toLowerCase().includes(searchText);
+      }
+      return values;
+      
     });
    }
   }
