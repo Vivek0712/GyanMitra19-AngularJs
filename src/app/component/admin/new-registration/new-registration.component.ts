35d56f49114acd0109b0af6cfc4f5ad978a33f9e
Aravind Raj <aravinddevaraj777@gmail.com>
2019-02-05 13:35:16 +0530

    changes
    Aravind raj M

--- src/app/component/admin/new-registration/new-registration.component.ts
+++ src/app/component/admin/new-registration/new-registration.component.ts
@@ -6,6 +6,7 @@ import { DepartmentService } from 'src/app/services/department/department.servic
 import { CollegeService } from 'src/app/services/college/college.service';
 import { DegreeService } from 'src/app/services/degree/degree.service';
 import { YearService } from 'src/app/services/year/year.service';
+import { CourseService } from 'src/app/services/course/course.service';
 declare var M: any;
 
 @Component({
@@ -14,15 +15,16 @@ declare var M: any;
   styleUrls: ['./new-registration.component.css']
 })
 export class NewRegistrationComponent implements OnInit {
-  constructor(private yearService: YearService, private degreeservice: DegreeService,private collegeservice:CollegeService,private departmentservice: DepartmentService,private reg: UserregistrationService,private router: Router, private formBuilder: FormBuilder) { }
+  constructor(private yearService: YearService, private degreeservice: DegreeService, private collegeservice: CollegeService, private courseService: CourseService, private reg: UserregistrationService, private router: Router, private formBuilder: FormBuilder) { }
   registerForm: FormGroup;
+
   Button: any;
   submitted = false;
   departments: Array<any>;
   colleges: Array<any>;
   degrees: Array<any>;
-  years : Array<any>;
-  
+  years: Array<any>;
+
   ngOnInit() {
     this.createForm();
     this.getDepartments();
@@ -39,44 +41,44 @@ export class NewRegistrationComponent implements OnInit {
 
   get f() { return this.registerForm.controls; }
   //Create Form is Used to Initalize the Values the Form
-  createForm(){
-    this.Button="Register"
-    this.submitted=false;
+  createForm() {
+    this.Button = "Register"
+    this.submitted = false;
     this.registerForm = this.formBuilder.group({
-      email_id:['',Validators.required],
-      name:['',Validators.required],
-      password:['',Validators.required],
-      conpassword:['',Validators.required],
-      mobile_number:['',Validators.required],
-      degree_id:[''],
-      college_id:[''],
-      department_id:[''],
-      gender:[''],
-      year_id:['']
+      email_id: ['', Validators.required],
+      name: ['', Validators.required],
+      password: ['', Validators.required],
+      conpassword: ['', Validators.required],
+      mobile_number: ['', Validators.required],
+      degree_id: [''],
+      college_id: [''],
+      department_id: [''],
+      gender: [''],
+      year_id: ['']
     });
   }
   //The action performed After the Button is Pressed
-  onSubmit(values: NgForm){
-    this.submitted=true;
-    this.Button="Checking...";
-    if(this.registerForm.valid) {
+  onSubmit(values: NgForm) {
+    this.submitted = true;
+    this.Button = "Checking...";
+    if (this.registerForm.valid) {
       const email_id = this.registerForm.get('email_id').value;
       const password = this.registerForm.get('password').value;
       const conpassword = this.registerForm.get('conpassword').value;
-      const name =this.registerForm.get('name').value;
+      const name = this.registerForm.get('name').value;
       const mobile_number = this.registerForm.get('mobile_number').value;
-      const gender =this.registerForm.get('gender').value;
+      const gender = this.registerForm.get('gender').value;
       const college_id = this.registerForm.get('college_id').value;
       const degree_id = this.registerForm.get('degree_id').value;
       const department_id = this.registerForm.get('department_id').value;
       const year_id = this.registerForm.get('year_id').value;
-      
-      if(password !== conpassword){
+
+      if (password !== conpassword) {
         M.toast({ html: 'Passwords does not match', classes: 'rounded' });
         this.createForm();
       }
       else {
-        this.reg.createUser(name,college_id,department_id,degree_id,email_id,gender,mobile_number,password,year_id,true,"offline").subscribe((response: any) => {
+        this.reg.createUser(name, college_id, department_id, degree_id, email_id, gender, mobile_number, password, year_id, true, "offline").subscribe((response: any) => {
           if (response.error) {
             M.toast({ html: response.msg, classes: 'roundeds' });
           } else {
@@ -86,13 +88,13 @@ export class NewRegistrationComponent implements OnInit {
         });
       }
     }
-    else{
+    else {
       M.toast({ html: 'Please Check the Form', classes: 'rounded' });
-      this.Button="Register";
+      this.Button = "Register";
     }
   }
   getDepartments() {
-    this.departmentservice.readDepartment(0).subscribe((response: any) => {
+    this.courseService.readCourse(0).subscribe((response: any) => {
       this.departments = response;
     });
   }
